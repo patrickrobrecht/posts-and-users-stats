bac8359df75c1cad6910cd7b69759db701d8e3ea
Patrick Robrecht <mail@patrick-robrecht.de>
2016-09-21 21:56:29 +0200

    improved description in plugin's main file

--- posts-and-users-stats.php
+++ posts-and-users-stats.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Posts and Users Stats
 	Plugin URI: https://patrick-robrecht.de/wordpress/
-	Description: Statistics about the number of posts and users
+	Description: Statistics about the number of posts and users, provided as diagrams, tables and csv export.
 	Version: 0.3
 	Author: Patrick Robrecht
 	Author URI: https://patrick-robrecht.de/
